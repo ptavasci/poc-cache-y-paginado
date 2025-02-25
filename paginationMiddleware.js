fbfe9a12013197fbdb9db619bd2c98fe661a6537
Pablo Tavasci Dozo <ptavasci@gmail.com>
2025-02-25 09:03:36 -0300

    unknown variables and better logging + refactor

--- paginationMiddleware.js
+++ paginationMiddleware.js
@@ -4,7 +4,7 @@ function paginationMiddleware(req, res, next) {
     const schema = Joi.object({
         limit: Joi.number().integer().positive().optional().default(5),
         offset: Joi.number().integer().min(0).optional().default(0)
-    });
+    }).unknown(true); // Allow unknown query parameters
 
     const { error, value } = schema.validate(req.query);
 
@@ -14,27 +14,32 @@ function paginationMiddleware(req, res, next) {
 
     req.pagination = value;
 
-    let originalJson = res.json.bind(res);
+    const originalJson = res.json.bind(res);
 
-    res.json = (data) => {
+    const paginatedData = (data) => {
         res.locals.originaldata = data;
 
         if (!Array.isArray(data)) {
             return originalJson(data);
         }
-
         const { limit, offset } = req.pagination;
         const paginatedData = data.slice(offset, offset + limit);
+        const totalPages = Math.ceil(data.length / limit);
+        const currentPage = Math.floor(offset / limit) + 1;
+
+        console.info('Getting page %i from %i pages (total elements %i)', currentPage, totalPages, data.length);
 
         res.set('X-Total-Count', data.length);
-        res.set('X-Total-Pages', Math.ceil(data.length / limit));
+        res.set('X-Total-Pages', totalPages);
         res.set('X-Elements-Per-Page', limit);
-        res.set('X-Current-Page', Math.floor(offset / limit) + 1);
+        res.set('X-Current-Page', currentPage);
         originalJson(paginatedData);
     };
 
+    res.json = paginatedData;
+
     if (req.cacheddata) {
-        res.json(req.cacheddata);
+        paginatedData(req.cacheddata);
     } else {
         next();
     }
