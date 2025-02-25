8e94959beeaf1165644e3467dfb266ce1e2f1463
Pablo Tavasci Dozo <ptavasci@gmail.com>
2025-02-25 09:02:38 -0300

    Use traceId for index, 30 minutes cache

--- cacheMiddleware.js
+++ cacheMiddleware.js
@@ -1,34 +1,34 @@
 const crypto = require('crypto');
 
 async function cacheMiddleware(req, res, next) {
-    const { limit, offset, ...restQuery } = req.query;
-    const hash = crypto.createHash('md5').update(JSON.stringify(restQuery)).digest('hex');
-    const cacheKey = `cache:${req.originalUrl.split('?')[0]}:${hash}`;
+  const { limit, offset, traceId, ...restQuery } = req.query;
+  const hash = crypto.createHash('md5').update(JSON.stringify({ ...restQuery, traceId })).digest('hex');
+  const cacheKey = `cache:${req.originalUrl.split('?')[0]}:${hash}`;
 
-    //falta mejorar el hash para que se base en un identificador de traza que provenga desde el front para diferenciar las requests que provienen del paginado de las que provienen del botón buscar
+  try {
+    const cachedData = await req.redisClient.get(cacheKey);
 
-    try {
-        const cachedData = await req.redisClient.get(cacheKey);
-
-        if (cachedData) {
-            console.info('Data retrieved from Redis cache');
-            req.cacheddata = JSON.parse(cachedData);
-            return next();
-        } else {
-            console.info('Data not found in Redis cache');
-        }
-
-        res.sendResponse = res.json;
-        res.json = (body) => {
-            req.redisClient.setEx(cacheKey, 60, JSON.stringify(res.locals.originaldata)); // Cache for 60 seconds
-            res.sendResponse(body);
-        };
-
-        next();
-    } catch (err) {
-        console.error('Error retrieving data from Redis:', err);
-        next();
+    if (cachedData) {
+      console.info('Full data retrieved from Redis cache');
+      req.cacheddata = JSON.parse(cachedData);
+      return next();
+    } else {
+      console.info('Full data not found in Redis cache');
     }
+
+    res.sendResponse = res.json;
+    res.json = (body) => {
+      if (res.locals.originaldata) {
+        req.redisClient.setEx(cacheKey, 30 * 60, JSON.stringify(res.locals.originaldata)); // Cache for 30 minutes
+      }
+      res.sendResponse(body);
+    };
+
+    next();
+  } catch (err) {
+    console.error('Error retrieving data from Redis:', err);
+    next();
+  }
 }
 
 module.exports = cacheMiddleware;
